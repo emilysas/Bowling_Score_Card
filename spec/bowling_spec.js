9fb56764635dc35e2bbba4d6c828d733382a97f9
Emily Sas <emily.bronwen.sas@gmail.com>
2015-01-24 20:06:53 +0000

    adding test - game ends after 10 frames

--- spec/bowling_spec.js
+++ spec/bowling_spec.js
@@ -2,11 +2,10 @@ describe("Bowling Score Card", function() {
 
 	var game;
   var player;
-  var frame1;
 
   beforeEach(function() {
-  	game = new Game(player);
   	player = new Player("Emily");
+  	game = new Game(player);
   });
 
   describe("Game", function() {
@@ -16,8 +15,13 @@ describe("Bowling Score Card", function() {
   	});
 
   	it("moves from frame 1 through to 10", function() {
-  		game.play
+  		game.play();
   		expect(game.frames.length).toEqual(9)
+  	});
+
+  	it("ends after frame 10", function() {
+  		for (var i = 1; i <= 10; i++) {game.frames.shift};
+  		expect(game.play()).toEqual("You have bowled all 10 frames, Game Over");
   	});
 
   });
@@ -25,28 +29,28 @@ describe("Bowling Score Card", function() {
   describe("Player", function() {
 
     it("can hit between 0 and 10 pins with their first throw", function() {
-      expect(game.play).toBeLessThan(11);
+      expect(game.play()).toBeLessThan(11);
     });
 
     it("can hit any remaining pins if they have a second throw", function() {
     	game.frames[0].pins = 6;
-    	expect(game.play).toBeLessThan(7);
+    	expect(game.play()).toBeLessThan(7);
     });
 
   });
 
   describe("Frame", function() {
 
-  	// it("ends when a player has bowled twice", function() {
-  	// 	player.bowl(frame1);
-  	// 	player.bowl(frame1);
-  	// 	expect(player.bowl(frame1)).toEqual("You've had two bowls already this frame, please start a new frame");
-  	// });
+  	it("ends when a player has bowled twice", function() {
+  		player.bowl(frame1);
+  		player.bowl(frame1);
+  		expect(player.bowl(frame1)).toEqual("You've had two bowls already this frame, please start a new frame");
+  	});
 
-  	// it("ends when a player scores a strike", function() {
-  	// 	frame1.pins = 0;
-  	// 	expect(player.bowl(frame1)).toEqual("All your pins are down, please start a new frame");
-  	// });
+  	it("ends when a player scores a strike", function() {
+  		frame1.pins = 0;
+  		expect(player.bowl(frame1)).toEqual("All your pins are down, please start a new frame");
+  	});
 
   });
 
--- src/bowling.js
+++ src/bowling.js
@@ -3,6 +3,7 @@ var Player = function(name) {
 };
 
 var Game = function(player) {
+	this.player = player;
 	this.frames = [frame1 = new Frame(), 
 								frame2 = new Frame(), 
 								frame3 = new Frame(), 
@@ -16,25 +17,17 @@ var Game = function(player) {
 }
 
 var Frame = function() {
-	var pins = 10;Vz`z`ZAaV
-	var bowl = 0;
-
-	this.remainingPins = function() {
-		return pins;
-	};
-
-	this.bowlCount = function() {
-		return bowl;
-	};
+	this.pins = 10;
+	this.bowl = 0;
 };
 
 var ScoreCard = function(){
 };
 
 Player.prototype.bowl = function(frameNumber) {
-	if (frameNumber.bowlCount === 2) {
+	if (frameNumber.bowl === 2) {
 		return "You've had two bowls already this frame, please start a new frame"
-	} else if (frameNumber.remainingPins === 0) {
+	} else if (frameNumber.pins === 0) {
 		return "All your pins are down, please start a new frame"
 	} else {
 		knockedDownPins = this.knockDownPins(frameNumber);	
@@ -44,15 +37,15 @@ Player.prototype.bowl = function(frameNumber) {
 };
 
 Player.prototype.knockDownPins = function(frameNumber) {
-	knockedDownPins = Math.floor(Math.random()*(frameNumber.remainingPins+1));
-	frameNumber.remainingPins -= knockedDownPins;
-	frameNumber.bowlCount += 1;
+	knockedDownPins = Math.floor(Math.random()*(frameNumber.pins+1));
+	frameNumber.pins -= knockedDownPins;
+	frameNumber.bowl += 1;
 	return knockedDownPins;
 }
 
 Game.prototype.play = function() {
-	frame = this.frames.shift;
-	result = player.bowl(frame);
+	frame = this.frames.shift();
+	result = this.player.bowl(frame);
 	return result;
 }
 
